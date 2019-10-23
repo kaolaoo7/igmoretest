99da9896235793f97a039d391b283bdd6f7778a4
yangting <406763876@qq.com>
2019-10-23 21:34:37 +0800

    Initial commit

---
+++ src/main/java/com/qf/MyTest.java
@@ -0,0 +1,11 @@
+package com.qf;
+
+/**
+ * @Auther YangTing
+ * @Date 2019/10/23
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("hello....mytest");
+    }
+}
